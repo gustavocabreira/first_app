8541be8fd5b50eddf464e0b3634fc0dd185db6af
Gustavo Cabreira <gustavo.softdev@gmail.com>
2022-04-17 20:03:51 -0300

    creating a stateful widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,24 +1,54 @@
-import 'package:first_app/home_controller.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var controller = HomeController.of(context);
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('hahahaha')
+//       ),
+//       body: Center(
+//         child: Text('Contador: ${controller.count}')
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(Icons.add),
+//         onPressed: () {
+//           controller.increment();
+//         },
+//       ),
+//     );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    var controller = HomeController.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('hahahaha')
       ),
       body: Center(
-        child: Text('Contador: ${controller.count}')
+        child: Text('Contador: $counter')
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          controller.increment();
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
